7eb5bf2c47a4476e12bab34d21dc34390168f3b2
AbbyIsCoding <abigail_rochelle24@milton.edu>
2022-10-28 11:26:53 -0400

    Second (?) commit of this project. In this version I was able to print all 3x3 board variations, their result boards, and the move to make in order to get to each result board (in (row, column) format)

--- src/Square.java
+++ src/Square.java
@@ -2,7 +2,7 @@ public class Square {
     public int a;
     public int b;
     public int c;
-    public int x;
+
 
     public static void main(String[] args) {
         Square s = new Square();
@@ -23,8 +23,8 @@ public class Square {
     public void printResultBoards(int A, int B, int C) {
         for (int c2 = C - 1; c2 >= 0; c2--) { // third col
 
-            System.out.println(A + " " + B + " " + c2 + " move to make: " + c2 + "," + (C - 1));
-            System.out.println(C);
+            System.out.println(A + " " + B + " " + c2 + " move to make: " + c2 + "," + 2);
+            //System.out.println(C);
 
         }
 
@@ -32,11 +32,11 @@ public class Square {
         for (int b2 = B - 1; b2 >= 0; b2--) { // second col
 
             if (b2 < C) {
-                System.out.println(A + " " + b2 + " " + b2 + " move to make: " + b2 + "," + (B - 1));
+                System.out.println(A + " " + b2 + " " + b2 + " move to make: " + b2 + "," + 1);
             } else {
-                System.out.println(A + " " + b2 + " " + C + " move to make: " + b2 + "," + (B - 1));
+                System.out.println(A + " " + b2 + " " + C + " move to make: " + b2 + "," + 1);
             }
-            System.out.println(B);
+            //System.out.println(B);
 
         }
 
@@ -44,16 +44,15 @@ public class Square {
 
 
             if (a2 < B && a2 < C) {
-                System.out.println(a2 + " " + a2 + " " + a2 + " move to make: " + a2 + "," + (A - 1));
-
+                System.out.println(a2 + " " + a2 + " " + a2 + " move to make: " + a2 + "," + 0);
             }
             else if (a2 < B) {
-                System.out.println(a2 + " " + a2 + " " + C + " move to make: " + a2 + "," + (A - 1));
+                System.out.println(a2 + " " + a2 + " " + C + " move to make: " + a2 + "," + 0);
             }else {
 
-                System.out.println(a2 + " " + B + " " + C + " move to make: " + a2 + "," + (A - 1));
+                System.out.println(a2 + " " + B + " " + C + " move to make: " + a2 + "," + 0);
             }
-            System.out.println(A);
+           // System.out.println(A);
 
 
         }
